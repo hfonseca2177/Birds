188bb61104be0e4450df9eba938a848225dad317
hfonseca2177 <hfonseca21@gmail.com>
2020-08-24 11:19:47 -0300

    First inclusion

---
+++ Assets/Bird.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    Vector3 _initialPosition;
+    private bool _birdWasLaunched;
+    private float _timeSittingAround;
+    private const float PositiveSceneEdgeLimit = 10;
+    private const float NegativeSceneEdgeLimit = -20;
+    private const float IddleTimeLimit = 3;
+
+    [SerializeField] private float _launchPower = 500;
+
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);        
+
+        if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if(isOffLimit(transform.position.y) || isOffLimit(transform.position.x)            
+            || _timeSittingAround > IddleTimeLimit)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }        
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
+        GetComponent<LineRenderer>().enabled = false;
+
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+
+    private bool isOffLimit(float axisPosition)
+    {
+        return axisPosition > PositiveSceneEdgeLimit || axisPosition < NegativeSceneEdgeLimit;
+    }
+}
---
+++ Assets/Monster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _cloudParticiplePrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if (bird != null)
+        {
+            Instantiate(_cloudParticiplePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        Monster monster = collision.collider.GetComponent<Monster>();
+        if(monster != null)
+        {
+            return;
+        }
+
+        bool collisionOnTop = collision.contacts[0].normal.y < -0.5;
+        if (collisionOnTop)
+        {
+            Instantiate(_cloudParticiplePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+}
